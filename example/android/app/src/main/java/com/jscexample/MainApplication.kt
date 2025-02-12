46e62d405cbb4365630f01c35b356af7989d4657
Kudo Chien <ckchien@gmail.com>
2025-02-13 02:13:37 +0800

    Apply suggestions from code review
    Co-authored-by: Nicola Corti <corti.nico@gmail.com>

--- example/android/app/src/main/java/com/jscexample/MainApplication.kt
+++ example/android/app/src/main/java/com/jscexample/MainApplication.kt
@@ -31,8 +31,8 @@ class MainApplication : Application(), ReactApplication {
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
 
-        override fun getJavaScriptExecutorFactory(): JavaScriptExecutorFactory {
-          return JSCExecutorFactory(packageName, AndroidInfoHelpers.getFriendlyDeviceName())
+        override fun getJavaScriptExecutorFactory(): JavaScriptExecutorFactory =
+          JSCExecutorFactory(packageName,  AndroidInfoHelpers.getFriendlyDeviceName())
         }
       }
 
